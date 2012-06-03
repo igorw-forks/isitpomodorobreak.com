10aa4cd7be1e5787cfac4a2aaff4d24e091297f2
Marcel Hauri <marcel.hauri@liip.ch>
2012-06-03 10:50:11 +0200

    remove all for plain website

--- web/index.php
+++ web/index.php
@@ -1,13 +1,1 @@
-<?php
-
-require_once __DIR__.'/../vendor/autoload.php';
-
-$app = new Silex\Application();
-
-$app->register(new Silex\Provider\TwigServiceProvider(), array(
-    'twig.path' => __DIR__.'/../views',
-));
-$app->get('/', function() use ($app) {
-  return $app['twig']->render('index.twig');
-});
-$app->run();
+isitpomodorobreak.com
