06a6e3cc295202845c4dfe10bfc04eff6cd17ff2
Marcel Hauri <marcel.hauri@liip.ch>
2012-06-03 09:44:13 +0200

    update views

--- web/index.php
+++ web/index.php
@@ -4,8 +4,10 @@ require_once __DIR__.'/../vendor/autoload.php';
 
 $app = new Silex\Application();
 
-$app->get('/', function() {
-    return 'isitpomodorobreak.com';
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__.'/../views',
+));
+$app->get('/', function() use ($app) {
+  return $app['twig']->render('index.twig');
 }); 
-
 $app->run();
