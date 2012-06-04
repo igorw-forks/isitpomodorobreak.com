3b57acbe31a3dd94769f6940e21fa800f43881d8
Marcel Hauri <marcel.hauri@liip.ch>
2012-06-04 09:59:31 +0200

    open external links in a new window

--- web/js/main.js
+++ web/js/main.js
@@ -1,5 +1,16 @@
 jQuery(document).ready(function() {
 
+    $('a').each(function() {
+       var a = new RegExp('/' + window.location.host + '/');
+       if(!a.test(this.href)) {
+           $(this).click(function(event) {
+               event.preventDefault();
+               event.stopPropagation();
+               window.open(this.href, '_blank');
+           });
+       }
+    });
+
     setInterval( function() {
         var time = parseInt($("#time").html(), 10),
             newTime,
@@ -24,5 +35,4 @@ jQuery(document).ready(function() {
         }
         $("#time").html(time+1);
     },1000);
-
 });
