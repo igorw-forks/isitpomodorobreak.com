1bd9431749a45b407b1a2147a59b0727fb0dcba1
Marcel Hauri <marcel.hauri@liip.ch>
2012-06-04 09:44:49 +0200

    remove title status

--- web/js/main.js
+++ web/js/main.js
@@ -19,10 +19,8 @@ jQuery(document).ready(function() {
 
         if((minutes >= 25 && minutes <= 29) || minutes >= 55 && minutes <= 59) {
             $("#status").attr('class', 'yes').html('yes');
-            document.title = 'Is it Pomodoro break? Yes, it is!';
         } else if(minutes == 30 || minutes == 0) {
             $("#status").attr('class', 'no').html('no');
-            document.title = 'Is it Pomodoro break? No, it isn\'t!';
         }
         $("#time").html(time+1);
     },1000);
