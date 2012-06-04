f93fbd4e34034f1845d0720cdd8f33f1d7c48fb0
Marcel Hauri <marcel.hauri@liip.ch>
2012-06-04 08:47:14 +0200

    fix status change on mobile devices

--- web/js/main.js
+++ web/js/main.js
@@ -2,10 +2,21 @@ jQuery(document).ready(function() {
 
     setInterval( function() {
         var time = parseInt($("#time").html(), 10),
-            newTime = new Date(time*1000),
-            hours    = newTime.getHours(),
-            minutes  = newTime.getMinutes(),
-            seconds  = newTime.getSeconds();
+            newTime,
+            hours,
+            minutes,
+            seconds;
+
+        if(isNaN(time)) {
+            newTime = new Date();
+        } else {
+            newTime = new Date(time * 1000);
+        }
+
+        hours    = newTime.getHours();
+        minutes  = newTime.getMinutes();
+        seconds  = newTime.getSeconds();
+
         if((minutes >= 25 && minutes <= 29) || minutes >= 55 && minutes <= 59) {
             $("#status").attr('class', 'yes').html('yes');
         } else if(minutes == 30 || minutes == 0) {
